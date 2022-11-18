a081654e3585e993243fbab5e3709df4e48d4240
Andresm98 <andresm17u7@hotmail.com>
2022-11-17 19:49:56 -0500

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,10 +135,12 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
+
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -155,6 +157,7 @@ class TestAccountService(TestCase):
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
 
+
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
@@ -162,6 +165,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
 
     def test_delete_account(self):
         """It should Delete an Account"""
